5a89e3234904442a9b9e437450df67d30f5f8708
brodrigues2005 <rodriguesbrett85@gmail.com>
2025-03-17 20:53:56 -0700

    set up basic structure for balance panel component

---
+++ src/frontend/src/components/BalancePanel.jsx
@@ -0,0 +1,13 @@
+import "../styling/BalancePanel.css";
+
+function BalancePanel({ checkings, savings, total }) {
+  return (
+    <>
+      <div>
+        <h1> Balance Panel</h1>
+      </div>
+    </>
+  );
+}
+
+export default BalancePanel;
