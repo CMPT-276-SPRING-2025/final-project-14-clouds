bf2b5ca7bdd518a05e8caa6366d57a655224b8dc
Audrey Safikhani <safikhani.parasto@gmail.com>
2025-03-19 17:57:38 -0700

    added navigation manu

--- src/frontend/src/pages/Analytics.jsx
+++ src/frontend/src/pages/Analytics.jsx
@@ -1,11 +1,28 @@
-import "../styles.css";
+import "../styling/menu.css";
+import { Link } from "react-router-dom";
+import Logo from "../styling/Removal-906.png";
+
 
 function Analytics() {
   return (
     <>
-      <h1>Analytics and Report</h1>
-    </>
-  );
+    <div className="nav">
+    </div>
+
+    <nav className="menu">
+      <ul>
+        <li>
+          <img src={Logo} alt="profile logo" className="logo" />
+        </li>
+        <li><Link to="/Dashboard">Dashboard</Link></li>
+        <li><Link to="/AccountActivity">Account Activity</Link></li>
+        <li><Link to="/Goals">My Goals</Link></li>
+        <li><Link to="/Analytics">Analytics</Link></li>
+        <li><Link to="/Advice">Advice</Link></li>
+      </ul>
+    </nav>
+  </>
+);
 }
 
 export default Analytics;
