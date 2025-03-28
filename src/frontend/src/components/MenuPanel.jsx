134bd4c2ef1e6933b4b1321fab142300b196ffb7
AGRIM JOSHI <joshiagrim1@gmail.com>
2025-03-28 12:21:12 -0700

    Made panel layout and updated css for Account activity panel

---
+++ src/frontend/src/components/MenuPanel.jsx
@@ -0,0 +1,28 @@
+import "../styling/menu.css";
+import { Link } from "react-router-dom";
+import Logo from "../styling/Removal-906.png";
+
+
+function MenuPanel() {
+  return (
+    <>
+
+      <div className="nav">
+        <div className="logo-container">
+        <img src={Logo} alt="profile logo" className="logo" />
+      </div>
+      </div>
+      <nav className="menu">
+        <ul>
+          <li><Link to="/Dashboard">Dashboard</Link></li>
+          <li><Link to="/AccountActivity">Account Activity</Link></li>
+          <li><Link to="/Goals">My Goals</Link></li>
+          <li><Link to="/Analytics">Analytics</Link></li>
+          <li><Link to="/Advice">Advice</Link></li>
+        </ul>
+      </nav>
+
+    </>
+  );
+}
+export default MenuPanel;
