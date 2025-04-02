beb267db312e3a6529c39810a0a72141d6f16d09
brodrigues2005 <rodriguesbrett85@gmail.com>
2025-04-02 16:05:19 -0700

    updated cors origin, frogot https

--- src/backend/server.js
+++ src/backend/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 //Allow frontend requests
 app.use(cors({
-    origin: "final-project-14-clouds-1.onrender.com",
+    origin: "https://final-project-14-clouds-1.onrender.com",
     methods: "GET,POST",
     credentials: true,
 }));
