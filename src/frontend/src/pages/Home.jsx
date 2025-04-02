2d75e36a565dabfec9f1602e02e071083864e3b3
brodrigues2005 <rodriguesbrett85@gmail.com>
2025-04-02 15:53:30 -0700

    testing if reac router dom works well with render for frontend

--- src/frontend/src/pages/Home.jsx
+++ src/frontend/src/pages/Home.jsx
@@ -1,25 +1,28 @@
 import "../styling/index.css";
 import Logo from "../styling/Removal-906.png";
-//import BalancePanel from "../components/BalancePanel"; 
+import { Link } from "react-router-dom";
+//import BalancePanel from "../components/BalancePanel";
 function Home() {
   return (
     <>
-    <div className="welcome">
-      <div className="glowing-circle"></div>
-      <div className="start-button">Get started now</div>
-      <div className="background-container">
-        <div className="profile">
-          <img src={Logo} alt="profile pic" className="profile-circle" />
+      <div className="welcome">
+        <div className="glowing-circle"></div>
+
+        <Link to="/Dashboard">
+          <div className="start-button">Get started now</div>
+        </Link>
+
+        <div className="background-container">
+          <div className="profile">
+            <img src={Logo} alt="profile pic" className="profile-circle" />
+          </div>
+          <h1>
+            Your Secure <br /> Gateway to Simple, <br /> Smart Finances
+          </h1>
         </div>
-        <h1>Your Secure <br /> Gateway to Simple, <br /> Smart Finances</h1>
       </div>
-    </div>
-
-    
-
-
-  </>
-);
+    </>
+  );
 }
 
 export default Home;
