950fc610398e1e0b6a2aeb1baa99e8a91c18b1d2
brodrigues2005 <rodriguesbrett85@gmail.com>
2025-04-05 14:19:18 -0700

    login feature should be complete

--- src/frontend/src/pages/Login.jsx
+++ src/frontend/src/pages/Login.jsx
@@ -1,24 +1,33 @@
 import "../styling/Login.css";
-import Plaid from "../styling/plaid.jpg"
-import P2 from "../styling/p2.png"
+import Plaid from "../styling/plaid.jpg";
+import P2 from "../styling/p2.png";
+import LoginPanel from "../components/loginPanel";
+import { useState, useEffect } from "react";
 
 function Login() {
+  const [status, setStatus] = useState("none");
+
   return (
-    <div className="login">
-      <div className="log-button">LOGIN WITH PLAID</div>
-      <div className="glowing-circle"></div>
-      <div className="background-container">
-        <div className="profile">
-          <img src={P2} alt="log in page pic" className="profile-circle" />
+    <>
+      <div className="login">
+        <div className="log-button" onClick={() => setStatus("block")}>
+          LOGIN
         </div>
-        <div className="plaid">
-          <img src={Plaid} alt="plaid logo" className="plaid-logo" />
+
+        <div className="background-container">
+          <div className="profile">
+            <img src={P2} alt="log in page pic" className="profile-circle" />
+          </div>
+
+          <h1>
+            Welcome, <br /> We are glad to see you
+          </h1>
         </div>
-        <h1>
-          Welcome, <br /> We are glad to see you
-        </h1>
       </div>
-    </div>
+      <div>
+        <LoginPanel displayStatus={status} setter={setStatus} />
+      </div>
+    </>
   );
 }
 export default Login;
